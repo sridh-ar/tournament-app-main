acec517980d61e3568ba86b1bc34ecb0c3586232
Sridhar <b.sridhar@bahwancybertek.com>
2024-02-12 23:14:34 +0530

    temp change

--- app/api/upload/route.js
+++ app/api/upload/route.js
@@ -1,5 +1,6 @@
 import { writeFile } from "fs/promises";
 import path from "path";
+import { promises as fs } from 'fs';
 
 export async function POST(request) {
   const data = await request.formData();
@@ -11,11 +12,13 @@ export async function POST(request) {
   const bytes = await file.arrayBuffer();
   const buffer = Buffer.from(bytes);
 
+  console.log(file);
   console.log("Current directory:",path.join("public", "uploads", file.name));
   console.log("Path:",path.join(__dirname,"public", "uploads", file.name));
   // const path1 = path.join("public", "uploads", file.name);
-  const filePath = path.join("public", file.name);
-  await writeFile(filePath, buffer)
+  // const filePath = path.join("public", file.name);
+  // await writeFile(filePath, buffer)
+  await fs.writeFile(`public/uploads/${file.name}`, buffer);
 
   return new Response(JSON.stringify('File Upload Success'));
 }
