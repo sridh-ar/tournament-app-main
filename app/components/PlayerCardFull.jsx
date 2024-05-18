a884bfd0549b5a7761a7ce07a57fcc707620fcb8
Sridhar <sridharkeerthiga99@gmail.com>
2024-05-18 17:12:15 +0530

    change

--- app/components/PlayerCardFull.jsx
+++ app/components/PlayerCardFull.jsx
@@ -19,7 +19,7 @@ export default function PlayersCardFull({
 }) {
   const [isBgLoading, setBgLoading] = useState(true);
   const [isPlayerLoading, setPlayerLoading] = useState(true);
-  
+
   return (
     <div className="top-0 left-0 absolute w-screen h-screen z-50">
       <div className="relative bg-white">
@@ -33,7 +33,7 @@ export default function PlayersCardFull({
           loading="eager"
           onLoadingComplete={() => setBgLoading(false)}
         />
-        <div className="bg-black w-screen h-screen opacity-70 absolute" />
+        <div className="bg-black w-screen h-screen opacity-50 absolute" />
 
         {/* Close Icon */}
         <Button containerClassName="absolute top-8 cursor-pointer right-8 w-[40px] h-[40px] z-50">
@@ -50,52 +50,57 @@ export default function PlayersCardFull({
         <Meteors number={30} />
 
         {/* Modal Body */}
-        <div className="absolute text-white w-full h-screen grid items-end p-5 px-20 grid-cols-5 ">
-          <section className="col-span-1">
+        <div className="absolute text-white w-full h-screen grid items-end p-5 px-20 grid-cols-7 ">
+          {/* Player Name */}
+          <section className="col-span-2 flex flex-col items-start">
             <p className="text-7xl capitalize">{name.split(' ')[0]}</p>
             {name.split(' ')[1] && <p className="text-5xl capitalize">{name.replace(name.split(' ')[0], '').trim()}</p>}
             <p className="text-3xl capitalize">{team}</p>
           </section>
-          <div className="col-span-2 flex justify-center">
+
+          {/* Player Image */}
+          <div className="col-span-3 w-full flex justify-center">
             <img
               src={image}
               alt="Rounded avatar"
-              className="w-[80%] max-h-[90vh] object-contain shadow-xl"
+              className="w-[90%] max-h-[90vh] object-contain shadow-xl"
               loading="eager"
               onLoadingComplete={() => setPlayerLoading(false)}
             />
           </div>
-         
 
-          <table className="text-2xl col-span-2">
-            <tr className="bg-gray-800">
-              <td className="font-semibold p-2 px-5">Serial No</td>
-              <td>{id}</td>
-            </tr>
-            <tr>
-              <td className="font-semibold p-2 px-5">Role</td>
-              <td>{role}</td>
-            </tr>
-            <tr className="bg-gray-800">
-              <td className="font-semibold p-2 px-5">Batting Style</td>
-              <td>{battingStyle}</td>
-            </tr>
-            <tr>
-              <td className="font-semibold p-2 px-5">Bowling Style</td>
-              <td>{bowlingStyle}</td>
-            </tr>
-            <tr className="bg-gray-800">
-              <td className="font-semibold p-2 px-5">Team</td>
-              <td>{team}</td>
-            </tr>
-            <tr>
-              <td className="font-semibold p-2 px-5">Area</td>
-              <td>{area}</td>
-            </tr>
-          </table>
+          {/* Table */}
+          <section className=" col-span-2 flex flex-col items-end">
+            <table className="text-2xl">
+              <tr className="bg-gray-800">
+                <td className="font-semibold p-2 px-5">Serial No</td>
+                <td>{id}</td>
+              </tr>
+              <tr>
+                <td className="font-semibold p-2 px-5">Role</td>
+                <td>{role}</td>
+              </tr>
+              <tr className="bg-gray-800">
+                <td className="font-semibold p-2 px-5">Batting Style</td>
+                <td>{battingStyle}</td>
+              </tr>
+              <tr>
+                <td className="font-semibold p-2 px-5">Bowling Style</td>
+                <td>{bowlingStyle}</td>
+              </tr>
+              <tr className="bg-gray-800">
+                <td className="font-semibold p-2 px-5">Team</td>
+                <td>{team}</td>
+              </tr>
+              <tr>
+                <td className="font-semibold p-2 px-5">Area</td>
+                <td>{area}</td>
+              </tr>
+            </table>
+          </section>
           <div className="absolute w-[90%] h-1 shadow-[0_-5px_20px_#fff] bottom-2 ml-[5%]" />
           <div className="absolute w-[90%] h-1 shadow-[0_-5px_20px_#fff] bottom-2 ml-[5%]" />
-        </div>        
+        </div>
       </div>
       {(isBgLoading && isPlayerLoading) && <div className="relative bg-white w-screen h-screen flex items-center justify-center">
         <img src="/loading2.svg" alt="loader" className="w-36 h-36" />
